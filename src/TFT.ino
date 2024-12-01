8d7be7a13a98bd3e58a2464baf663044678fdb2a
crlab3 <mpoximer@yahoo.com>
2024-12-01 20:32:16 +0100

    updated small things in GUI number background, dpf background etc.

--- src/TFT.ino
+++ src/TFT.ino
@@ -13,14 +13,14 @@
 #include "TFT_eSPI.h" // https://github.com/Bodmer/TFT_eSPI
 #include "digital7font.h"
 
-#define DEBUGMODE 1
+#define DEBUGMODE 0
 #define OBD_ERROR_COUNT_MAX 100
 // Pin definitions for RGB LED CI,DI
 #define DATA_PIN 3
 #define CLOCK_PIN 13
 
 TFT_eSPI tft = TFT_eSPI();
-TFT_eSprite coolantTempSprite = TFT_eSprite(&tft);
+TFT_eSprite mainSprite = TFT_eSprite(&tft);
 IPAddress server(192, 168, 0, 10);
 WiFiClient client;
 ELM327 myELM327;
@@ -63,142 +63,162 @@ uint8_t pidIndex = 0;
 uint8_t numPids = 3;
 uint16_t obdErrorCount = 0;
 
-
 obdData obdArray[3] = {obdEngineCoolantTempData,obdEngineOilTempData,obdRegenerationStatusData};
-
 ObdConnectionState connectionState=ELM_NOT_CONNECTED;
 
 void setup() 
 {
-  // Setup LED Configuration of a single RGB LED on Board
-  FastLED.addLeds<APA102, LED_DI_PIN, LED_CI_PIN, BGR>(&onBoardRGBLED, 1);
-  onBoardRGBLED = CRGB(255,0,0);
-  FastLED.show();
-  
-  Serial.begin(115200);
-  Serial.println("Hello T-Dongle-S3");
+    // Setup LED Configuration of a single RGB LED on Board
+    FastLED.addLeds<APA102, LED_DI_PIN, LED_CI_PIN, BGR>(&onBoardRGBLED, 1);
+    onBoardRGBLED = CRGB(255,255,255);
+    FastLED.show();
+    
+    // Start serial debugging
+    #if DEBUGMODE
+    Serial.begin(115200);
+    Serial.println("Hello T-Dongle-S3");
+    #endif
+    
+    // Initialise TFT
+    tft.init();
+    tft.setRotation(2);
+    tft.fillScreen(TFT_DARKGREY);
+    
+    // Set TFT background LED to on
+    pinMode(TFT_LEDA_PIN, OUTPUT);
+    digitalWrite(TFT_LEDA_PIN, 0);
 
-  pinMode(TFT_LEDA_PIN, OUTPUT);
-  // Initialise TFT
-  tft.init();
-  tft.setRotation(2);
-  tft.fillScreen(TFT_DARKGREY);
+    // Sprite creation
+    mainSprite.createSprite(tft.width(),tft.height());
 
-  digitalWrite(TFT_LEDA_PIN, 0);
-  tft.setTextFont(1);
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    // Create TFT timer
+    timerTFT = timerBegin(timerTFTID, timerTFTPrescaler, true);
+    timerAttachInterrupt(timerTFT, &timerTFTISR, true);
+    timerAlarmWrite(timerTFT, timerTFTThreshold, true);
+    timerAlarmEnable(timerTFT);
+    timerStart(timerTFT);
 
-  coolantTempSprite.createSprite(tft.width(),tft.height());
-  
-  timerTFT = timerBegin(timerTFTID, timerTFTPrescaler, true);
-  timerAttachInterrupt(timerTFT, &timerTFTISR, true);
-  timerAlarmWrite(timerTFT, timerTFTThreshold, true);
-  timerAlarmEnable(timerTFT);
-  timerStart(timerTFT);
+    // Create timer for OBD updates
+    timerA = timerBegin(timerAID, timerAPrescaler, true);
+    timerAttachInterrupt(timerA, &timerAISR, true);
+    timerAlarmWrite(timerA, timerAThreshold, true);
+    timerAlarmEnable(timerA);
+    timerStart(timerA);
 
-
-  timerA = timerBegin(timerAID, timerAPrescaler, true);
-  timerAttachInterrupt(timerA, &timerAISR, true);
-  timerAlarmWrite(timerA, timerAThreshold, true);
-  timerAlarmEnable(timerA);
-  timerStart(timerA);
-
-
-  // Initialize WiFi Connection
-  WiFi.mode(WIFI_AP);
-  WiFi.begin(SSID);
+    // Initialize WiFi Connection
+    WiFi.mode(WIFI_AP);
+    WiFi.begin(SSID);
 }
 
 void loop() 
-{ // Put your main code here, to run repeatedly:
-button.tick();
+{ 
+    // Call button callback function to trigger interrupt
+    button.tick();
 
-if(timerAFlag && initCycle)
-{
-    timerAFlag = 0;
-    if(WiFi.status() == WL_CONNECTED && connectionState == ELM_NOT_CONNECTED)
+    // Build connection to WiFi OBD module
+    if(timerAFlag && initCycle)
     {
-        #if DEBUGMODE
-            Serial.println("Connection state = ELM_WIFI_CONNECTED");
-        #endif
-        connectionState = ELM_WIFI_CONNECTED;
-    }
-    if(connectionState == ELM_WIFI_CONNECTED)
-    {
-        uint32_t connResp = client.connect(server, 35000);
-        if(connResp)
-        {    
-            #if DEBUGMODE
-                Serial.println("Connection state = ELM_CLIENT_CONNECTED");
-            #endif
-            connectionState = ELM_CLIENT_CONNECTED;
-        }
-    }
-    if(connectionState == ELM_CLIENT_CONNECTED)
-    {
-        bool elmStatus = myELM327.begin(client, false, 2000);
-        if(elmStatus)
+        timerAFlag = 0;
+        if(WiFi.status() == WL_CONNECTED && connectionState == ELM_NOT_CONNECTED)
         {
             #if DEBUGMODE
-                Serial.println("Connection state = ELM_DEVICE_CONNECTED");
+                Serial.println("Connection state = ELM_WIFI_CONNECTED");
             #endif
-            connectionState = ELM_DEVICE_CONNECTED;
-            initCycle = 0;
+            connectionState = ELM_WIFI_CONNECTED;
+        }
+        if(connectionState == ELM_WIFI_CONNECTED)
+        {
+            uint32_t connResp = client.connect(server, 35000);
+            if(connResp)
+            {    
+                #if DEBUGMODE
+                    Serial.println("Connection state = ELM_CLIENT_CONNECTED");
+                #endif
+                connectionState = ELM_CLIENT_CONNECTED;
+            }
+        }
+        if(connectionState == ELM_CLIENT_CONNECTED)
+        {
+            bool elmStatus = myELM327.begin(client, false, 2000);
+            if(elmStatus)
+            {
+                #if DEBUGMODE
+                    Serial.println("Connection state = ELM_DEVICE_CONNECTED");
+                #endif
+                connectionState = ELM_DEVICE_CONNECTED;
+                initCycle = 0;
+            }
+        }
+    } 
+
+    // WiFi Connection is stabilized, start OBD command
+    if(connectionState == ELM_DEVICE_CONNECTED)
+    {
+        int8_t ELMRxState = obdGetData(pidIndex);
+        if(ELMRxState == ELM_SUCCESS)
+        {
+        // Go to the next pidIndex
+        pidIndex++;
+        // Always modulo the pidIndex
+        pidIndex %= numPids;
+        }
+        if(ELMRxState == OBD_ERROR_COUNT_MAX)
+        {
+        // Display some kind of error on the TFT display
+        // Reset the esp32
+        ESP.restart();
+        }
+        #if DEBUGMODE
+            Serial.print("ELMRxState: ");
+            Serial.println(ELMRxState);
+            Serial.print("Number of OBD errors: ");
+            Serial.println(obdErrorCount);
+            Serial.print("Current PID Index: ");
+            Serial.println(pidIndex);
+        #endif
+        // tft.pushImage(0, 0, 160, 80, (uint16_t *)gImage_dpfregen);
+    }
+
+    // Update the TFT Screen with a main sprite
+    if(timerTFTFlag)
+    {
+        mainSprite.loadFont(digital7font);
+        mainSprite.fillScreen(TFT_BLACK);
+        mainSprite.setTextDatum(MC_DATUM);
+        mainSprite.setTextSize(2);
+        mainSprite.setTextColor(TFT_BLACK, TFT_BLUE,true);
+        mainSprite.drawNumber(obdArray[0].pidData,tft.width()/2,tft.height()/2,4);
+        mainSprite.setTextColor(TFT_BLACK, TFT_ORANGE,true);
+        mainSprite.drawNumber(obdArray[1].pidData,tft.width()/2,tft.height()/2+50,4);
+        mainSprite.setTextColor(TFT_YELLOW,false);
+        if(obdArray[2].pidData>0)
+        {
+            mainSprite.drawString("DPF",tft.width()/2,tft.height()/2-50,4);    
+        }
+        else
+        {
+            mainSprite.drawString("---",tft.width()/2,tft.height()/2-50,4); 
+        }
+        mainSprite.pushSprite(0,0);
+        
+        // Turn the RGB LED to the correct color value
+        if(obdArray[0].pidData<80 && obdArray[1].pidData<80)
+        {
+            onBoardRGBLED = CRGB(0,0,255);
+            FastLED.show();
+        } 
+        if(obdArray[0].pidData>=80 && obdArray[1].pidData>=80 && obdArray[0].pidData<110 && obdArray[1].pidData<110)
+        {
+            onBoardRGBLED = CRGB(0,255,0);
+            FastLED.show();
+        }
+        if(obdArray[0].pidData>=110 && obdArray[1].pidData>=110)
+        {
+            onBoardRGBLED = CRGB(255,0,0);
+            FastLED.show();
         }
     }
-} 
 
-if(connectionState == ELM_DEVICE_CONNECTED)
-{
-    int8_t ELMRxState = obdGetData(pidIndex);
-    if(ELMRxState == ELM_SUCCESS)
-    {
-      // Go to the next pidIndex
-      pidIndex++;
-      // Always modulo the pidIndex
-      pidIndex %= numPids;
-    }
-    if(ELMRxState == OBD_ERROR_COUNT_MAX)
-    {
-      // Display some kind of error on the TFT display
-      // Reset the esp32
-      ESP.restart();
-    }
-    #if DEBUGMODE
-        Serial.print("ELMRxState: ");
-        Serial.println(ELMRxState);
-        Serial.print("Number of OBD errors: ");
-        Serial.println(obdErrorCount);
-        Serial.print("Current PID Index: ");
-        Serial.println(pidIndex);
-    #endif
-    // tft.pushImage(0, 0, 160, 80, (uint16_t *)gImage_dpfregen);
-}
-
-if(timerTFTFlag)
-{
-  coolantTempSprite.loadFont(digital7font);
-  coolantTempSprite.fillScreen(TFT_BLACK);
-  coolantTempSprite.setTextDatum(MC_DATUM);
-  coolantTempSprite.setTextColor(TFT_BLUE);
-  coolantTempSprite.setTextSize(2);
-  coolantTempSprite.drawNumber(obdArray[0].pidData,tft.width()/2,tft.height()/2,4);
-  coolantTempSprite.setTextColor(TFT_ORANGE);
-  coolantTempSprite.drawNumber(obdArray[1].pidData,tft.width()/2,tft.height()/2+50,4);
-  coolantTempSprite.setTextColor(TFT_YELLOW);
-  coolantTempSprite.drawNumber(obdArray[2].pidData,tft.width()/2,tft.height()/2-50,4);
-  coolantTempSprite.pushSprite(0,0);
-  if(obdArray[0].pidData>=80 && obdArray[1].pidData>=80)
-  {
-    onBoardRGBLED = CRGB(0,255,0);
-    FastLED.show();
-  }
-  else
-  {
-    onBoardRGBLED = CRGB(0,0,255);
-    FastLED.show();
-  }
-}
 }
 
 void IRAM_ATTR timerAISR()
